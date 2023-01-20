8896dc32424f3ca549af19e08905b2f9b2ace2bb
Nathan Campos <nathan@innoveworkshop.com>
2023-01-20 13:04:53 +0000

    Added all of the scaffolding for the library

--- src/pickle.c
+++ src/pickle.c
@@ -9,6 +9,93 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void pickle_test(const char *test) {
-	printf("Testing PickLE %s\n", test);
+/**
+ * Opens an existing or brand new PickLE document file for parsing/saving.
+ *
+ * @param doc   Pointer to a PickLE document object.
+ * @param fname Document file path.
+ *
+ * @return PICKLE_OK if the operation was successful. PICKLE_ERROR_FILE if an
+ *         error occurred while trying to open the file.
+ */
+pickle_err_t pickle_open(pickle_doc_t *doc, const char *fname) {
+	return PICKLE_ERROR_NOT_IMPL;
+}
+
+/**
+ * Closes the file handle for a PickLE document.
+ *
+ * @param doc PickLE document object to have its file handle closed.
+ *
+ * @return PICKLE_OK if the operation was successful. PICKLE_ERROR_FILE if an
+ *         error occurred while trying to close the file.
+ */
+pickle_err_t pickle_close(pickle_doc_t *doc) {
+	return PICKLE_ERROR_NOT_IMPL;
+}
+
+/**
+ * Parses a property header item at the beginning of the document.
+ *
+ * @param doc  PickLE document object.
+ * @param prop Property object to be populated by this function. Will be set to
+ *             NULL if there isn't a valid property to parse.
+ *
+ * @return PICKLE_OK if a property was parsed. PICKLE_PARSED_BLANK if a blank
+ *         line was found. PICKLE_FINISHED_PARSING when there are no more
+ *         properties to be parsed. PICKLE_ERROR_PARSING if the line we tried to
+ *         parse was malformed.
+ *
+ * @see pickle_parse_document
+ */
+pickle_err_t pickle_parse_property(pickle_doc_t *doc, pickle_property_t *prop) {
+	return PICKLE_ERROR_NOT_IMPL;
+}
+
+/**
+ * Parses a category line in the document.
+ *
+ * @param doc PickLE document object.
+ * @param cat Category object to be populated by this function. Will be set to
+ *            NULL if there isn't a valid category to parse.
+ *
+ * @return PICKLE_OK if a category was parsed. PICKLE_PARSED_BLANK if a blank
+ *         line was found. PICKLE_ERROR_PARSING if the line we tried to
+ *         parse was malformed.
+ *
+ * @see pickle_parse_document
+ */
+pickle_err_t pickle_parse_category(pickle_doc_t *doc, pickle_category_t *cat) {
+	return PICKLE_ERROR_NOT_IMPL;
+}
+
+/**
+ * Parses a component item in the document.
+ *
+ * @param doc  PickLE document object.
+ * @param comp Component object to be populated by this function. Will be set to
+ *             NULL if there isn't a valid component to parse.
+ *
+ * @return PICKLE_OK if a component was parsed. PICKLE_PARSED_BLANK if a blank
+ *         line was found. PICKLE_FINISHED_PARSING when there are no more
+ *         components to be parsed. PICKLE_ERROR_PARSING if the line we tried to
+ *         parse was malformed.
+ *
+ * @see pickle_parse_document
+ */
+pickle_err_t pickle_parse_component(pickle_doc_t *doc, pickle_component_t *comp) {
+	return PICKLE_ERROR_NOT_IMPL;
+}
+
+/**
+ * Parses a whole document at once and populates the contents field of the
+ * pickle_doc_t structure.
+ *
+ * @param doc Opened PickLE document object.
+ *
+ * @return PICKLE_OK if everything was parsed fine. PICKLE_ERROR_PARSING if
+ *         something in the document couldn't be parsed.
+ */
+pickle_err_t pickle_parse_document(pickle_doc_t *doc) {
+	return PICKLE_ERROR_NOT_IMPL;
 }
--- src/pickle.h
+++ src/pickle.h
@@ -8,6 +8,73 @@
 #ifndef _PICKLE_H
 #define _PICKLE_H
 
-void pickle_test(const char *test);
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+/* PickLE parser status codes. */
+typedef enum {
+	PICKLE_FINISHED_PARSING = -2,
+	PICKLE_PARSED_BLANK,
+	PICKLE_OK,
+	PICKLE_ERROR_FILE,
+	PICKLE_ERROR_PARSING,
+	PICKLE_ERROR_UNKNOWN,
+	PICKLE_ERROR_NOT_IMPL
+} pickle_err_t;
+
+/* Reference designator list. */
+typedef struct {
+	int length;
+	char **refdes;
+} refdes_list_t;
+
+/* PickLE category object. */
+typedef struct {
+	char *name;
+} pickle_category_t;
+
+/* PickLE component object. */
+typedef struct {
+	bool picked;
+	char *name;
+	char *value;
+	char *description;
+	char *package;
+	refdes_list_t refdes;
+
+	pickle_category_t *category;
+} pickle_component_t;
+
+/* PickLE property item object. */
+typedef struct {
+	char *name;
+	char *value;
+} pickle_property_t;
+
+/* PickLE document handle. */
+typedef struct {
+	const char *fname;
+	FILE *fh;
+
+	pickle_property_t **properties;
+	int len_properties;
+
+	pickle_category_t **categories;
+	int len_categories;
+
+	pickle_component_t **components;
+	int len_components;
+} pickle_doc_t;
+
+/* PickLE document operations. */
+pickle_err_t pickle_open(pickle_doc_t *doc, const char *fname);
+pickle_err_t pickle_close(pickle_doc_t *doc);
+
+/* PickLE parsing operations. */
+pickle_err_t pickle_parse_property(pickle_doc_t *doc, pickle_property_t *prop);
+pickle_err_t pickle_parse_category(pickle_doc_t *doc, pickle_category_t *cat);
+pickle_err_t pickle_parse_component(pickle_doc_t *doc, pickle_component_t *comp);
+pickle_err_t pickle_parse_document(pickle_doc_t *doc);
 
 #endif /* _PICKLE_H */
